070e8f26d6a3612bc315cef54f831a47c546c525
Marco Beier <marco-beier@t-online.de>
2020-04-17 17:48:12 +0200

    Initialize repository with easy-ui5

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/mrb/workbench/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.mrb.workbench.controller.Main", {});
+});
