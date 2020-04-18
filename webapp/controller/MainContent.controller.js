ecadf4558a6080026c4c858e86af6ddb81b63aaa
Marco Beier <marco-beier@t-online.de>
2020-04-18 11:57:56 +0200

    feat: rough integration of the codeeditor control and split of app into a shell

---
+++ webapp/controller/MainContent.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+  "com/mrb/workbench/controller/BaseController"
+], function (
+  BaseController
+) {
+  "use strict";
+
+  return BaseController.extend("com.mrb.workbench.controller.MainContent", {});
+});
