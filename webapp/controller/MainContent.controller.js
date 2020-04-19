8b294b6bbf1d97d033ca9e2249ee918cc0821818
Marco Beier <marco-beier@t-online.de>
2020-04-19 19:09:25 +0200

    feat: when clicking a language it gets displayed above the CE

--- webapp/controller/MainContent.controller.js
+++ webapp/controller/MainContent.controller.js
@@ -1,9 +1,27 @@
-sap.ui.define([
-  "com/mrb/workbench/controller/BaseController"
-], function (
+sap.ui.define(["com/mrb/workbench/controller/BaseController"], function (
   BaseController
 ) {
   "use strict";
 
-  return BaseController.extend("com.mrb.workbench.controller.MainContent", {});
+  return BaseController.extend("com.mrb.workbench.controller.MainContent", {
+    onInit: function () {
+      this._codeEditor = this.byId("aCodeEditor");
+      this._ceTitle = this.byId("ceTitle");
+      //manual initialization - TODO: replace by json binding
+      this._codeEditor.setType("abap");
+      this._updateCeTitle("ABAP");
+    },
+
+    onItemPress: function (oEvt) {
+      var boundItemValues = oEvt
+        .getParameters()
+        .listItem.getBindingContext()
+        .getProperty();
+      this._codeEditor.setType(boundItemValues.value);
+      this._updateCeTitle(boundItemValues.name);
+    },
+    _updateCeTitle: function(sName){
+      this._ceTitle.setText(sName);
+    }
+  });
 });
