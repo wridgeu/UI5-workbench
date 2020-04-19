e4c5ff738ba38b55a5057d49ea78e5bb3ad828b1
Marco Beier <marco-beier@t-online.de>
2020-04-19 21:17:43 +0200

    changed the way you choose a theme / syntax highlighting

--- webapp/controller/MainContent.controller.js
+++ webapp/controller/MainContent.controller.js
@@ -5,30 +5,30 @@ sap.ui.define(["com/mrb/workbench/controller/BaseController"], function (
 
   return BaseController.extend("com.mrb.workbench.controller.MainContent", {
     onInit: function () {
-      this._codeEditor = this.byId("aCodeEditor");
-      this._ceTitle = this.byId("ceTitle");
+      this._codeEditor  = this.byId("aCodeEditor");
+      this._languSelect = this.byId("aLanguageSelector");
+      this._themeSelect = this.byId("aThemeSelector")
+      
       //manual initialization - TODO: replace by json binding
       this._codeEditor.setType("abap");
-      this._updateCeTitle("ABAP");
+      this._languSelect.setSelectedKey("abap");
     },
-    // TODO: Menu Item handlers
-    onMenuSaves: function () {},
-    onMenuTheme: function () {},
-    onMenuLanguage: function () {},
+    onMenuSaves: function () {
+       // TODO: add saving functionality - browser
+    },
+    onThemeSelection: function(oEvt){
+      var boundItemValues = oEvt.getParameters().selectedItem.getBindingContext().getProperty();
+      this._codeEditor.setColorTheme(boundItemValues.name);
+    },
+    onLanguageSelection: function(oEvt){
+      var boundItemValues = oEvt.getParameters().selectedItem.getBindingContext().getProperty();
+      this._codeEditor.setType(boundItemValues.value)
+    },
     _updateListBinding: function(){
       // TODO: dynamically upate the binding of list per menu-button
     },
     onListItemPress: function (oEvt) {
       // TODO: dynamic bind -> CE Type, Theme or ReadSavedItems
-      var boundItemValues = oEvt
-        .getParameters()
-        .listItem.getBindingContext()
-        .getProperty();
-      this._codeEditor.setType(boundItemValues.value);
-      this._updateCeTitle(boundItemValues.name);
-    },
-    _updateCeTitle: function (sName) {
-      this._ceTitle.setText(sName);
     }
   });
 });
