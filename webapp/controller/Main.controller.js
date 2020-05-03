c943c5135abe747a798d7a3bec09eed52f825fe5
Marco Beier <marco-beier@t-online.de>
2020-05-03 23:00:10 +0200

    pretty print

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,6 +1,6 @@
-sap.ui.define([
-  "com/mrb/workbench/controller/BaseController"
-], function(Controller) {
+sap.ui.define(["com/mrb/workbench/controller/BaseController"], function (
+  Controller
+) {
   "use strict";
 
   return Controller.extend("com.mrb.workbench.controller.Main", {});
