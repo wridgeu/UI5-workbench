3895249a37c4936a2db5f80a13218ae2b19860a7
Marco Beier <marco-beier@t-online.de>
2020-05-12 23:09:29 +0200

    extracted storage extension into external file; removed not needed device

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,11 +1,10 @@
 sap.ui.define(
   [
     "sap/ui/core/UIComponent",
-    "sap/ui/Device",
     "com/mrb/workbench/model/models",
-    "sap/ui/util/Storage",
+    "com/mrb/workbench/model/utilStorage",
   ],
-  function (UIComponent, Device, models, UI5Storage) {
+  function (UIComponent, models, UI5Storage) {
     "use strict";
 
     return UIComponent.extend("com.mrb.workbench.Component", {
@@ -21,39 +20,20 @@ sap.ui.define(
       init: function () {
         // call the base component's init function
         UIComponent.prototype.init.apply(this, arguments);
-
         // enable routing
         this.getRouter().initialize();
-
         // set the device model
         this.setModel(models.createDeviceModel(), "device");
-        
-        //add "read-all"-method to prototype
-        UI5Storage.prototype.getItems = function () {
-          if (!window.localStorage) throw "No localStorage support for this browser";
-          var items = {};
-          for (var i = 0; i < window.localStorage.length; i++) {
-            var itemKey = window.localStorage.key(i),
-              itemValue = window.localStorage.getItem(itemKey);
-
-            Object.defineProperty(items, itemKey, {
-              value: itemValue,
-              enumerable: true
-            });
-          }
-          return items;
-        };
-        
         // initialize UI5Storage-Instance for localStorage with prefix 'codeEditor'
         this._workbenchStorage = new UI5Storage("local", "codeEditor");
       },
-      /**
-       * Returns the initialized storage Instance.
-       * @public
-       * @returns {sap.ui.util.Storage}
-       */
+      //return the initialized storage instance
       getStorageInstance: function () {
-        return this._workbenchStorage;
+        if ("localStorage" in window) {
+          return this._workbenchStorage;
+        } else {
+          throw "No support for Local Storage API";
+        }
       },
     });
   }
